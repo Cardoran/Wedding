85721df9429b5852f184ed6451ff83fa704c8e43
Cardoran <ccardoran@gmail.com>
2026-07-13 23:04:04 +0200

    add more security to website

--- netlify/edge-functions/auth.js
+++ netlify/edge-functions/auth.js
@@ -14,6 +14,18 @@ function getCookie(request, name) {
   }
   
   export default async (request, context) => {
+    
+    const pathname = new URL(request.url).pathname;
+  
+    // öffentlich zugängliche Seiten
+    if (
+      pathname === "/" ||
+      pathname === "/index.html" 
+    //   pathname.startsWith("/.netlify/functions/login")
+    ) {
+      return context.next();
+    }
+
     const auth = getCookie(request, "auth");
   
     if (auth !== "true") {
@@ -25,3 +37,33 @@ function getCookie(request, name) {
   
     return context.next();
   };
+
+//   export default async (request, context) => {
+//     const pathname = new URL(request.url).pathname;
+  
+//     // öffentlich zugängliche Seiten
+//     if (
+//       pathname === "/" ||
+//       pathname === "/index.html" ||
+//       pathname.startsWith("/.netlify/functions/login")
+//     ) {
+//       return context.next();
+//     }
+  
+//     const cookie = request.headers.get("cookie") || "";
+  
+//     const auth = cookie
+//       .split(";")
+//       .map(x => x.trim())
+//       .find(x => x.startsWith("auth="))
+//       ?.split("=")[1];
+  
+//     if (auth !== "true") {
+//       return Response.redirect(
+//         new URL("/index.html", request.url),
+//         302
+//       );
+//     }
+  
+//     return context.next();
+//   };
